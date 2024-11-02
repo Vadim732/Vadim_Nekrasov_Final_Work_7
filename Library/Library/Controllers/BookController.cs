a2f8c4da8d9fab91cef6e4d287228d3abfed0089
Nekrasov Vadim <vadim65337.68@gmail.com>
2024-11-02 13:12:01 +0600

    Added and implemented a controller for the Book model. Added and implemented the display of the list of all books as cards in the Index view. Added and implemented the ability to add a new book in the Create view. Added and implemented the ability to edit a book in the Edit view. Added and implemented the ability to delete a selected book. Added all necessary validation. Updated _Layout and added links to all required pages. Removed the Home controller along with its views. Set the default page to the Index view of the Book controller.

---
+++ Library/Library/Controllers/BookController.cs
@@ -0,0 +1,92 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers;
+
+public class BookController : Controller
+{
+    private readonly LibraryContext _context;
+
+    public BookController(LibraryContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        List<Book> books = await _context.Books.Include(b => b.Category).OrderByDescending(b => b.DateAdded).ToListAsync();
+        return View(books);
+    }
+    
+    public async Task<IActionResult> Create()
+    {
+        ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(Book book)
+    {
+        if (ModelState.IsValid)
+        {
+            book.DateAdded = DateTime.UtcNow;
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(book);
+    }
+    
+    public async Task<IActionResult> Details(int bookId)
+    {
+        Book book = await _context.Books.Include(b => b.Category).FirstOrDefaultAsync(b => b.Id == bookId);
+        if (book != null)
+        {
+            return View(book);
+        }
+
+        return NotFound();
+    }
+
+    public async Task<IActionResult> Edit(int bookId)
+    {
+        Book book = await _context.Books.Include(b => b.Category).FirstOrDefaultAsync(b => b.Id == bookId);
+        ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
+        if (book != null)
+        {
+            return View(book);
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Book book)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Books.Update(book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(book);
+    }
+    
+    public async Task<IActionResult> Delete(int bookId)
+    {
+        Book book = await _context.Books.FirstOrDefaultAsync(b => b.Id == bookId);
+        if (book != null)
+        {
+            _context.Remove(book);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToAction("Index");
+    }
+}
